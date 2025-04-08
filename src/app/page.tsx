25e8f9d6aee190ff132006cf0143f63d4055b276
dopoto <dopoto@gmail.com>
2025-04-08 09:18:43 +0300

    fetch, filter users

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,16 +2,53 @@
 
 import { useRouter } from "next/navigation";
 import { useFetchUsers } from "@/hooks/useFetchUsers";
+import { useState } from "react";
 
 export default function UsersPage() {
+    const [searchTerm, setSearchTerm] = useState("");
+
     const router = useRouter();
     const { users } = useFetchUsers();
-    console.log(JSON.stringify(users));
+
+    const filteredUsers = users.filter(
+        (user) =>
+            user.firstName.toLowerCase().includes(searchTerm.toLowerCase()) ||
+            user.lastName.toLowerCase().includes(searchTerm.toLowerCase()) ||
+            user.email.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+
     return (
         <div className="container mx-auto flex min-h-screen flex-col p-4">
-            <header className="mb-6">header</header>
+            <header className="mb-6">
+                <input
+                    type="text"
+                    placeholder="Search User"
+                    className="pl-8"
+                    value={searchTerm}
+                    onChange={(e) => setSearchTerm(e.target.value)}
+                />
+            </header>
 
-            <main className="flex-1">TODO</main>
+            <main className="flex-1">
+                <table>
+                    <thead>
+                        <tr>
+                            <td>Email</td>
+                            <td>First name</td>
+                            <td>Last name</td>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {filteredUsers.map((user) => (
+                            <tr key={user.id}>
+                                <td>{user.email}</td>
+                                <td>{user.firstName}</td>
+                                <td>{user.lastName}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </main>
 
             <footer className="mt-6 flex justify-end">
                 <button onClick={() => router.push("/add-user")}>
--- src/hooks/useFetchUsers.ts
+++ src/hooks/useFetchUsers.ts
@@ -1,5 +1,6 @@
 "use client";
 
+import axios from "axios";
 import { useState, useEffect } from "react";
 
 interface User {
@@ -9,16 +10,41 @@ interface User {
     email: string;
 }
 
+interface ApiResponse {
+    users: User[];
+    total: number;
+    limit: number;
+    skip: number;
+}
+
 export function useFetchUsers() {
     const [users, setUsers] = useState<User[]>([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState<Error | null>(null);
 
     useEffect(() => {
         const fetchUsers = async () => {
-            setUsers(await Promise.resolve([{ id: 1 }, { id: 2 }] as User[]));
+            try {
+                setLoading(true);
+                const response = await axios.get<ApiResponse>(
+                    "https://dummyjson.com/users"
+                );
+                setUsers(response.data.users);
+                setError(null);
+            } catch (err) {
+                setError(
+                    err instanceof Error
+                        ? err
+                        : new Error("Could not fetch users.")
+                );
+                setUsers([]);
+            } finally {
+                setLoading(false);
+            }
         };
 
         fetchUsers();
     }, []);
 
-    return { users };
+    return { users, loading, error };
 }
